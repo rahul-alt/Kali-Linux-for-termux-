eb76d8a25b92e31c73404272fe4deee9ffbdcd8e
rahul-alt <60128717+rahul-alt@users.noreply.github.com>
2024-03-23 16:55:15 +0530

    KALI_INSTALLATION.bat

---
+++ KALI_INSTALLATION.bat
@@ -0,0 +1,10 @@
+pkg upadte && upgrade -y
+pkg install wget
+termux-setup-storage
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+ls
+chmod +x install-nethunter-termux
+./install-nethunter-termux
+1
+nethunter kex passwd
+nh kex &
